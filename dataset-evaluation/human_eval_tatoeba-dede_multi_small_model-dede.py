9e367a5845ba2af6ddf9c58c85feab736593db5c
nikolayVv <nikolaivalvasilev@gmail.com>
2023-05-26 02:31:33 +0200

    human eval pt.2 + evaluation notebooks

--- dataset-evaluation/human_eval_tatoeba-dede_multi_small_model-dede.py
+++ dataset-evaluation/human_eval_tatoeba-dede_multi_small_model-dede.py
@@ -15,7 +15,7 @@ tatoeba_dede_samples = tatoeba_dede.select(sample_ids)
 
 N_PC = 20
 
-model_dede = datasets.load_dataset("csv", data_files="../data/eval_table_mono_dede.csv", split='train')
+model_dede = datasets.load_dataset("csv", data_files="../data/eval_table_multi_small_dede.csv", split='train')
 sample_ids = random.sample(range(model_dede.num_rows), N_PC)
 model_dede_samples = model_dede.select(sample_ids)
 
@@ -30,4 +30,4 @@ pc_sents = list(map(pc_to_sententepairs, model_dede_samples.to_list()))
 
 sents, corpus_ids = hef.mix_corpora(tatoeba_sents, pc_sents)
 scores = hef.eval_cmd(sents, hef.square, hef.square_neg)
-hef.store_scores_to_file(sents, scores, corpus_ids, "human_eval_tatoeba-enen_model-dede_"+uname+".txt")
+hef.store_scores_to_file(sents, scores, corpus_ids, "human_eval_tatoeba-dede_multi_small_model-dede_"+uname+".txt")
--- dataset-evaluation/human_eval_tatoeba-enen_multi_model-enen.py
+++ dataset-evaluation/human_eval_tatoeba-enen_multi_model-enen.py
@@ -15,7 +15,7 @@ tatoeba_enen_samples = tatoeba_enen.select(sample_ids)
 
 N_PC = 20
 
-model_enen = datasets.load_dataset("csv", data_files="../data/eval_table_mono_enen.csv", split='train')
+model_enen = datasets.load_dataset("csv", data_files="../data/eval_table_multi_enen.csv", split='train')
 sample_ids = random.sample(range(model_enen.num_rows), N_PC)
 model_enen_samples = model_enen.select(sample_ids)
 
@@ -30,4 +30,4 @@ pc_sents = list(map(pc_to_sententepairs, model_enen_samples.to_list()))
 
 sents, corpus_ids = hef.mix_corpora(tatoeba_sents, pc_sents)
 scores = hef.eval_cmd(sents, hef.square, hef.square_neg)
-hef.store_scores_to_file(sents, scores, corpus_ids, "human_eval_tatoeba-enen_model-enen_"+uname+".txt")
+hef.store_scores_to_file(sents, scores, corpus_ids, "human_eval_tatoeba-enen_multi_model-enen_"+uname+".txt")
